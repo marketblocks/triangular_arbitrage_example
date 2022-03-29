387b329ec820051ea4c70a13af3bc6ee672e1a3e
Jordan Clark <jordan.clark181@gmail.com>
2022-03-29 09:39:43 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include <spdlog/spdlog.h>
+
+#include "runner/runner.h"
+#include "logging/logger.h"
+#include "strategies/tri_arb.h"
+
+int main()
+{
+	cb::runner<tri_arb_strategy> runner{};
+
+	try
+	{
+		runner.initialise();
+		runner.run();
+	}
+	catch (const std::exception& e)
+	{
+		cb::logger::instance().critical(e.what());
+	}
+
+	spdlog::shutdown();
+	return 0;
+}
